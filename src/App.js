d4f333c4af7e87b863d7ff4bbee950368a345508
Reja <rezanurrochmat@gmail.com>
2022-05-16 21:37:32 +0700

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// Functional component
 
+import Header from "./components/Header";
+
+// Class component
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const message = "Bayu jawara Gartic gan!";
+
+    return ( 
+      <div>
+        <Header />
+        <h1>Halo PEMUDA REMAS, {message} , {2 + 2}</h1>
+        <p>Latif pro player ayo dance</p>
+      </div>
+    );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+// import React, {Component} from 'react';
+
+// Class component
+// export class Header extends Component {
+//     render() {
+//         return <div>Rafi itu ganteng</div>
+//     }
+// };
+
+// Functional component
+const Header = () => {
+    return <div>Bayu suka makan pempek</div>
+};
+
+export default Header;
