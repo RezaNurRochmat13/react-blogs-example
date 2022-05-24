8e393e5d87953736287cb51002444b4e4bd9a7b1
Reja <rezanurrochmat@gmail.com>
2022-05-24 22:04:42 +0700

    Add popup oauth

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,25 @@
 import React, {Component} from 'react';
+import GoogleLogin from 'react-google-login';
 
 export default class Home extends Component {
+
+    captureResponse = (response) => {
+        console.info(response);
+    };
+
     render() {
         return(
-            <div>Home component is works</div>
+            <div>
+                Home component is works
+                <GoogleLogin 
+                clientId='449632885333-phbbp1kth1aenlvbmdf8jnepp26efehf.apps.googleusercontent.com'
+                buttonText='Login'
+                onSuccess={this.captureResponse} 
+                onFailure={this.captureResponse} 
+                cookiePolicy={'single_host_origin'}
+                responseType='code'
+                redirectUri='http://localhost:3000' />
+            </div>
         );
     }
 }
